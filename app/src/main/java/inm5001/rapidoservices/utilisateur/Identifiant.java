cb4282fb5ca43d9f5f55469db21022d2a9364034
FrancisBernier <blabka19@hotmail.com>
2016-10-15 02:17:29 -0400

    Ajout de la validation de l'aspec unique de l'attribut nomUtilisateur de la classe Identifiant avec BD APIDB_temporaire (inclu les modifications appropriées pour IdentificantTest).

--- app/src/main/java/inm5001/rapidoservices/utilisateur/Identifiant.java
+++ app/src/main/java/inm5001/rapidoservices/utilisateur/Identifiant.java
@@ -1,16 +1,38 @@
 package inm5001.rapidoservices.utilisateur;
 
+import inm5001.rapidoservices.MyException;
+
+import static inm5001.rapidoservices.utilisateur.ConstanteIdentifiant.*;
+import inm5001.rapidoservices.utilisateur.APIDB_temporaire.BDIdentifiant;
 /**
  * Created by Francis Bernier on 2016-10-10.
  */
 
 public class Identifiant {
 
-    String nomUtilisateur;
-    String motDePasse;
+    public String nomUtilisateur;
+    public String motDePasse;
 
-    public Identifiant(String nomUtilisateur, String motDePasse) {
-        this.nomUtilisateur = nomUtilisateur;
+    public Identifiant(String nomUtilisateur, String motDePasse) throws MyException {
+        TraiterNomUtilisateur(nomUtilisateur);
+        TraiterMotDePasse(motDePasse);
+    }
+//premier niveau d'abstraction
+    private void TraiterNomUtilisateur(String nomUtilisateur) throws MyException {
+        ValiderNomUtilisateurEstUnique(nomUtilisateur);
+    }
+
+    private void TraiterMotDePasse(String motDePasse) {
         this.motDePasse = motDePasse;
     }
+//deuxième niveau d'abstraction
+    private void ValiderNomUtilisateurEstUnique(String nomUtilisateur) throws MyException {
+        if (BDIdentifiant.GetUtilisateur(nomUtilisateur) != null) {
+            MyException e = new MyException(MESSAGE_NOMUTILISATEUR_PAS_UNIQUE);
+            throw e;
+        } else {
+            this.nomUtilisateur = nomUtilisateur;
+        }
+    }
+//troisième niveau d'abstraction
 }
