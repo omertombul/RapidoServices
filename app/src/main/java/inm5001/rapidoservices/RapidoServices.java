fb8a5ae07be7838f2ea0377167cc2b9b20c9a957
turkguy <turkguy@hotmail.com>
2016-10-22 20:53:56 -0400

    Package fix

--- app/src/main/java/inm5001/rapidoservices/RapidoServices.java
+++ app/src/main/java/inm5001/rapidoservices/RapidoServices.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 
 import inm5001.rapidoservices.utilisateur.*;
 import inm5001.rapidoservices.service.*;
-import inm5001.rapidoservices.baseDonnees.*;
+import inm5001.rapidoservices.BaseDonnees.*;
 import inm5001.rapidoservices.MyException;
 import inm5001.rapidoservices.APIDB_temporaire.BDIdentifiant;
 
