9b513694fb9532e72e469438ba53af77c42c04b0
turkguy <turkguy@hotmail.com>
2016-10-27 23:41:22 -0400

    fix Connection MySql via l'application , update de l'adresse du nouveau serveur mySql

--- app/src/main/java/inm5001/rapidoservices/InscriptionActivity.java
+++ app/src/main/java/inm5001/rapidoservices/InscriptionActivity.java
@@ -4,6 +4,7 @@ package inm5001.rapidoservices;
         import android.app.AlertDialog;
         import android.content.Intent;
         import android.os.Bundle;
+        import android.os.StrictMode;
         import android.view.View;
         import android.widget.Button;
         import android.widget.EditText;
@@ -43,11 +44,10 @@ public class InscriptionActivity extends Activity {
     AlertDialog.Builder dlgAlert  ;
     Orchestrateur orchestrateur;
 
-   // AlertDialog adb = new AlertDialog.Builder(this).create();
-
-
-
-   // username = (EditText)findViewById(R.id.editText);
+    Connection conn = null;
+    Statement stmt = null;
+    String SQL = null;
+    ResultSet rs = null;
 
 
 
@@ -68,6 +68,8 @@ public class InscriptionActivity extends Activity {
         dlgAlert.setMessage("Erreur dans un des champs");
         dlgAlert.setPositiveButton("OK", null);
         dlgAlert.setCancelable(true);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
 
 
         sEnregistrer.setOnClickListener(new View.OnClickListener() {
@@ -85,16 +87,17 @@ public class InscriptionActivity extends Activity {
                     user = new Utilisateur(identifiant,profile,listeServices,listeCompetences);
                     orchestrateur = new Orchestrateur();
                     System.out.println(user.identifiant.nomUtilisateur);
-                    //orchestrateur.creerUtilisateur(user);
-                    BdApi bd = new BdApi();
-                    bd.addUser(user);
+                    orchestrateur.creerUtilisateur(user);
+                    //BdApi bd = new BdApi();
+                    //bd.addUser(user);
+                   // testBd("INSERT INTO utilisateur VALUES('Name' ,'Allo123!' ,'NAME' ,'NAME' ,'a@a.com' ,'1' ,'1' ,'1' ,'5142321212');");
                     Intent troisiemeActivite = new Intent(InscriptionActivity.this, ProfilActivity.class);
                     System.out.println(nomUtilisteur.getText().toString());
                     troisiemeActivite.putExtra(key_userName,nomUtilisteur.getText().toString());
                     troisiemeActivite.putExtra(key_email,adresseCourrielProfil.getText().toString());
                     troisiemeActivite.putExtra(key_nom, nom.getText().toString());
                     troisiemeActivite.putExtra(key_prenom, prenom.getText().toString());
-                    //startActivity(troisiemeActivite);
+                    startActivity(troisiemeActivite);
 
                 }catch(MyException e){
 
@@ -108,5 +111,87 @@ public class InscriptionActivity extends Activity {
 
             }
         });
+    }
+
+
+    public void testBd(String sq){
+            SQL = sq;
+            conn = makeConnection();
+            stmt = makeStatement();
+
+    }
+
+    private Connection makeConnection() {
+
+        // connect to Omer MySQL
+        String url = "jdbc:mysql://localhost:8889/service";
+        String usr = "root";
+        String psw = "root";
+
+        // connect to UQAM MySQL
+        //String url = "jdbc:mysql://127.0.0.1/bd_ak791165?:3306";
+        //String usr = "ak791165";
+        //String psw = "uWoavCsH";
+
+        // register MySQL Connector/J with the DriverManager
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+        } catch (Exception ex) {
+            System.out.println("Error Connecting.");
+        }
+        // once the driver is registered, make connection
+        try {
+            conn = DriverManager.getConnection(url, usr, psw);
+        } catch (SQLException ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+        System.out.println("Made Connection");
+        return conn;
+    }
+
+    private Statement makeStatement() {
+        try {
+            stmt = conn.createStatement();
+        } catch (SQLException ex) {
+            System.out.println(ex + "Error making DB statement");
+        }
+        return stmt;
+    }
+
+    public void insertToDB() {
+        try {
+            stmt.executeUpdate(SQL);
+        } catch (SQLException ex) {
+            System.out.println(ex + "    Error Putting new Data into DB");
+        }
+    }
+
+    public ResultSet readFromDataBase() {
+        try {
+            rs = stmt.executeQuery(SQL);
+        } catch (SQLException ex) {
+            System.out.println(ex + "    Error Getting Data: rs");
+        }
+        return rs;
+    }
+
+    public void deleteInDataBase() {
+        try {
+            stmt.executeUpdate(SQL);
+        } catch (SQLException ex) {
+            System.out.println(ex + "    Error Deleting From DATAbase");
+        }
+    }
+
+    public void closeConnection() {
+        try {
+            conn.close();
+        } catch (SQLException ex) {
+            System.out.println(ex + "Error Closing Connection");
+        }
+        System.out.println("Connection closed");
     }
 }
