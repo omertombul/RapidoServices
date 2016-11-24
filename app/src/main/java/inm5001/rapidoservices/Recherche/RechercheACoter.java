67d44ff9db912bda25e8e95ed930a66fa29abd51
FrancisBernier <blabka19@hotmail.com>
2016-11-23 22:14:34 -0500

    Ajout du package recherche et de la classe RechercheACoter

---
+++ app/src/main/java/inm5001/rapidoservices/Recherche/RechercheACoter.java
@@ -0,0 +1,15 @@
+package inm5001.rapidoservices.recherche;
+
+/**
+ * Created by Francis Bernier on 2016-11-23.
+ */
+
+public class RechercheACoter {
+    String nomUtilisateurACoter;
+    String nomService;
+
+    public RechercheACoter(String nomUtilisateurACoter, String nomServiceA){
+        this.nomUtilisateurACoter = nomUtilisateurACoter;
+        this.nomService = nomService;
+    }
+}
